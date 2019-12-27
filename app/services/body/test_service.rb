415981a583dba05f3930641f446404f7754ccb76
starwarrs <yulikamiya@gmail.com>
2019-12-27 15:21:22 +0900

    wip

--- app/services/body/test_service.rb
+++ app/services/body/test_service.rb
@@ -65,7 +65,7 @@ module Body
               conn.post '/api/chat.postMessage',body.to_json, {"Content-type" => 'application/json',"Authorization"=>"Bearer #{ENV['SLACK_BOT_USER_TOKEN']}"}
         elsif @json[:event][:text].include?("database")
               showusers = User.all
-              p showusers
+              p showusers.name
               body = {
                 :token => ENV['SLACK_BOT_USER_TOKEN'],
                 :channel => @json[:event][:channel],
